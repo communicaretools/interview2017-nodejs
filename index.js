dd7ae537ff9f16f6b76b0400b1df71777633be08
Stein Jakob Nordbø <steinjak@gmail.com>
2017-06-06 21:00:30 +0200

    Changes to api to make vuejs client work correctly

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express()
 const json = require('express-json')
 const bodyParser = require('body-parser')
 
+app.set('etag', false)
+app.use(express.static('public'))
+app.use(function (req, res, next) {
+  res.header("Content-Type",'application/json');
+  next();
+});
 app.use(bodyParser.json());
 app.use(json());
-app.use(express.static('public'))
 
 app.use('/api/registrations', require('./server/registrations.js'))
 app.use('/api/studies', require('./server/studies.js'))
