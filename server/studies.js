9c189bf7fda79966117a768d561d799e4e27a774
Stein Jakob Nordbø <steinjak@gmail.com>
2017-06-07 09:28:54 +0200

    Cleaned up study api implementation

--- server/studies.js
+++ server/studies.js
@@ -1,24 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+const studies = [
+    {"id":"8a773720-d2a1-4d30-b2b2-7c822fd29aaa", "name":"Stressmestring", "groups": ["App","Pilot","Kontroll"]},
+    {"id":"69bda4c1-a4fe-4e68-907d-2ee2ded85edc", "name":"eSupport", "groups": ["Full intervensjon","Kun spørsmål og svar","Kontroll"]},
+    {"id":"34618a7e-e0d5-438c-96fe-5ba2bd4bfc9f","name":"Revma styrke", "groups": ["App","Kurs","Pilot","Kontroll"]}
+]
+
 router.get('/', function getAllStudies(req, res) {
-    res.send([
-        {"id":"8a773720-d2a1-4d30-b2b2-7c822fd29aaa", "name":"Stressmestring"},
-        {"id":"69bda4c1-a4fe-4e68-907d-2ee2ded85edc", "name":"eSupport"},
-        {"id":"34618a7e-e0d5-438c-96fe-5ba2bd4bfc9f","name":"Revma styrke"}
-    ])
+    res.send(studies.map(s => ({id: s.id, name: s.name})))
 })
 
 router.get('/:id/groups', function getGroupsForStudy(req, res) {
-    if (req.params.id == "8a773720-d2a1-4d30-b2b2-7c822fd29aaa") {
-        res.send(["App","Pilot","Kontroll"])
-    } else if (req.params.id == "69bda4c1-a4fe-4e68-907d-2ee2ded85edc") {
-        res.send(["Full intervensjon","Kun spørsmål og svar","Kontroll"])
-    } else if (req.params.id == "34618a7e-e0d5-438c-96fe-5ba2bd4bfc9f") {
-        res.send(["App","Kurs","Pilot","Kontroll"])
-    } else {
+    var study = studies.find(s => s.id == req.params.id)
+    if (!study) {
         res.sendStatus(404)
+        return
     }
+    res.send(study.groups)
 })
 
 module.exports = router
